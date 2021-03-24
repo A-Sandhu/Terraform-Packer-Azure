87465c5dfa71143d62c0d5b32d97e3e9f3f69b67
Amrit Sandhu <Amrit@sandhu.nz>
2021-03-24 20:49:23 +1300

    updated script

--- create_packer_image.sh
+++ create_packer_image.sh
@@ -7,3 +7,26 @@ az ad sp create-for-rbac --query "{ client_id: appId, client_secret: password, t
 
 # To authenticate to Azure, you also need to obtain your Azure subscription ID with az account show:
 az account show --query "{ subscription_id: id }"
+
+# To build images, you create a template as a JSON file. In the template, you define builders and provisioners that carry out the actual build process.
+Like I created vmss.json
+
+# Build the image by specifying your Packer template file as follows:
+packer build vmss.json
+
+# Create VM from Azure Image. 
+az vm create \
+    --resource-group myResourceGroup-vmss \
+    --name myVM \
+    --image myPackerImage \
+    --admin-username azureuser \
+    --generate-ssh-keys
+
+# To allow web traffic to reach your VM, open port 80 from the Internet with az vm open-port:
+az vm open-port \
+    --resource-group myResourceGroup \
+    --name myVM \
+    --port 80
+
+# Test VM and NGINX
+Now you can open a web browser and enter http://publicIpAddress in the address bar. Provide your own public IP address from the VM create process.
